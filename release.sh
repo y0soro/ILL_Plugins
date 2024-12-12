d55a2271193cdc504555b70ad7cdd903e2e472ce
y0soro <y0soro@protonmail.com>
2024-12-13 02:39:14 +0800

    chore: update release script

--- release.sh
+++ release.sh
@@ -7,21 +7,33 @@ DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 # see https://github.com/dotnet/runtime/issues/33745
 dotnet build --configuration Release -p:ContinuousIntegrationBuild=true -p:PublishTrimmed=false
 
+cd $DIR
+
+git_version() {
+    local prefix=$1
+    (
+        set -o pipefail
+        git describe --tags --abbrev=7 --match="${prefix}*" 2>/dev/null | sed 's/^[^-]*-//;s/\([^-]*-g\)/r\1/;s/-/./g' ||
+            printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short=7 HEAD)"
+    )
+}
+
 for i in src/*; do
     name="${i#src/}"
     name="${name%/}"
 
-    version=$(sed -n 's/.*<Version>\(.*\)<.*/\1/p' "src/${name}/"*.csproj | tr -d '\n')
+    # version=v$(sed -n 's/.*<Version>\(.*\)<.*/\1/p' "src/${name}/"*.csproj | tr -d '\n')
 
-    echo $name
+    version=$(git_version $name)
 
     tmp=$(mktemp -d)
-    cd $tmp
+    pushd $tmp
 
     install -D "${DIR}/artifacts/bin/${name}/release/"*.dll -t ./BepInEx/plugins
 
-    zipFile="$DIR/artifacts/${name}-v${version}.zip"
+    zipFile="$DIR/artifacts/${name}-${version}.zip"
 
     rm -f $zipFile
     zip -r $zipFile .
+    popd
 done
