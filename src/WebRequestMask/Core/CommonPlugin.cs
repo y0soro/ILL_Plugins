461a5ab3338d8ed6c369756b3ead03e190b3df0c
y0soro <y0soro@protonmail.com>
2026-09-21 05:00:52 +0800

    improve(WebRequestMask): mask a common prefix of upcheck URLs covering Amaloca

--- src/WebRequestMask/Core/CommonPlugin.cs
+++ src/WebRequestMask/Core/CommonPlugin.cs
@@ -60,10 +60,12 @@ public class CommonPlugin
             "Mask URL Prefixes",
             new UrlPrefixes(
                 [
-                    "https://upcheck.illgames.jp/product/svs/game/check.php",
+                    // https://upcheck.illgames.jp/product/svs/game/check.php
+                    // https://upcheck.illgames.jp/product/digitalcraft/game/tos_check.php
+                    // https://upcheck.illgames.jp/product/aicomi/game/check.php
+                    // https://upcheck.illgames.jp/product/amaloca/game/check.php
+                    "https://upcheck.illgames.jp/product/",
                     "https://download.illgames.jp/check/game/tos_check.php",
-                    "https://upcheck.illgames.jp/product/digitalcraft/game/tos_check.php",
-                    "https://upcheck.illgames.jp/product/aicomi/game/check.php",
                 ]
             ),
             "Always responds with HTTP 200 OK for URLs matching these prefixes, effectively blocks public Web traffic towards these URLs."
